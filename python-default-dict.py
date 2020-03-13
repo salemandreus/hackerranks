79fe2d06c4f285864e34b4552a245757b562b49a
salemandreus <salemandreus@gmail.com>
2020-03-13 12:21:38 +0200

    fixed - now allows reversed/repeated words in B

--- python-default-dict.py
+++ python-default-dict.py
@@ -6,25 +6,49 @@ def find_words(n, m):
     d = defaultdict(list)
 
     for i in range(len(n)):
-        for word in m:
-            if n[i] == word:
-                d[word].append(i + 1)
+        d[n[i]].append(i + 1)
 
-    return list(d.values())
+
+    for word in m:
+        yield d[word]
+
+    # return list(d.values())
 
 def test_empty_list():
     """ returns empty list """
     group_a = []
     group_b = []
 
-    assert find_words(group_a, group_b) == []
+    assert list(find_words(group_a, group_b)) == []
 
 def test_contains_all_one_group():
     """return all if all in one group"""
     group_a = ['a', 'a', 'a', 'a', 'a']
     group_b = ['a']
 
-    assert find_words(group_a, group_b) == [[1, 2, 3, 4, 5]]
+    assert list(find_words(group_a, group_b)) == [[1, 2, 3, 4, 5]]
+
+
+def test():
+    group_a = ['a', 'a', 'b', 'a', 'b']
+    group_b = ['a', 'b']
+    assert list(find_words(group_a, group_b)) == [[1, 2, 4], [3, 5]]
+
+    group_a = ['a', 'a', 'b', 'a', 'b']
+    group_b = ['b', 'a']
+    assert list(find_words(group_a, group_b)) == [[3, 5], [1, 2, 4]]
+
+    group_a = ['a', 'a', 'b', 'a', 'b']
+    group_b = ['a', 'a']
+    assert list(find_words(group_a, group_b)) == [[1, 2, 4], [1, 2, 4]]
+
+
+def test_group_b_element_not_found():
+    group_a = ['a', 'a', 'b', 'a', 'b']
+    group_b = ['a', 'b', 'c']
+
+    assert list(find_words(group_a, group_b)) == [[1, 2, 4], [3, 5], [-1]]
+
 
 
 def test_single_group():
@@ -32,19 +56,4 @@ def test_single_group():
     group_a = ['a', 'a', 'b', 'a', 'b']
     group_b = ['a']
 
-    assert find_words(group_a, group_b) == [[1, 2, 4]]
-
-
-def test():
-    group_a = ['a', 'a', 'b', 'a', 'b']
-    group_b = ['a', 'b']
-
-    assert find_words(group_a, group_b) == [[1, 2, 4], [3, 5]]
-
-
-def test_group_b_element_not_found():
-    group_a = ['a', 'a', 'b', 'a', 'b']
-    group_b = ['a', 'b', 'c']
-
-    assert find_words(group_a, group_b) == [[1, 2, 4], [3, 5], [-1]]
-
+    assert list(find_words(group_a, group_b)) == [[1, 2, 4]]
