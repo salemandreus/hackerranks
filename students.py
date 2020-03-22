37f74cd40d4e8620c5e658692b0bb7b243524e0b
salemandreus <salemandreus@gmail.com>
2020-03-22 10:22:23 +0200

    refactor, move things out of io function that shouldn't be there for students.py

--- students.py
+++ students.py
@@ -1,25 +1,17 @@
 from textwrap import dedent
 
 
+def get_second_lowest(lines):
+    marks = set()
+    for line in lines:
+        student, mark = line.split()
+        marks.add(mark)
 
-def append_item_to_list(n, s):
-    students.append([n, s])
+    return sorted(marks)
 
-    return students
-
-def get_second_lowest(st):
-    return st
-
-
-def test_append_item_to_list_single():
-    lines =dedent("""
-    Harry 37.21
-    """).splitlines()
-
-    assert append_item_to_list(lines) == 5
 
 def test_append_item_to_list_multiple():
-    lines =dedent("""
+    lines =dedent("""\
     Harry 37.21
     Berry 37.21
     Tina 37.2
@@ -27,9 +19,7 @@ def test_append_item_to_list_multiple():
     Harsh 39
     """).splitlines()
 
-    assert append_item_to_list(lines) == 5
-
-
+    assert get_second_lowest(lines) == 5
 
 
 if __name__ == '__main__':
@@ -38,6 +28,5 @@ if __name__ == '__main__':
         name = input()
         score = float(input())
         students.append([name, score])
-        # stud = append_item_to_list(name, score)
-        print(get_second_lowest(stud))
+        print(get_second_lowest(students))
 
